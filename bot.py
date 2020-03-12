5190cebf57e5bf8b07df1c18f6de34f2de60a37b
Renata Marques <renata.nrmarques@gmail.com>
2020-03-11 23:01:52 -0300

    syntax error correction

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
 import time
 
 class Bot:
     def __init__(self):
         self.message = "O valor disponível na caixinha é x"
-        self.groups = ["Codeminer - Work", "Codeminer42 -THE"]
         options = webdriver.ChromeOptions()
         options.add_argument('lang=pt-br')
         self.driver = webdriver.Chrome(executable_path=r'./chromedriver')
@@ -12,18 +13,17 @@ class Bot:
     def SendMessage(self):
         self.driver.get('https://web.whatsapp.com')
         time.sleep(30)
-        for group in self.groups:
-            group = self.driver.find_element_by_xpath(f"//span[@title='{group}']")
-            time.sleep(3)
-            group.click()
-            chat_box = self.driver.find_element_by_class_name('_13mgZ')
-            time.sleep(3)
-            chat_box.click()
-            chat_box.send_keys(self.message)
-            send_button = self.driver.find_element_by_xpath("//span[@data-icon='send']")
-            time.sleep(3)
-            send_button.click()
-            time.sleep(3)
+        group = self.driver.find_element_by_xpath(f"//span[@title='Renata Marques']")
+        time.sleep(3)
+        group.click()
+        chat_box = self.driver.find_element_by_class_name('_13mgZ')
+        time.sleep(3)
+        chat_box.click()
+        chat_box.send_keys(self.message)
+        send_button = self.driver.find_element_by_xpath("//span[@data-icon='send']")
+        time.sleep(3)
+        send_button.click()
+        time.sleep(3)
 
 wbot = Bot()
 wbot.SendMessage()
